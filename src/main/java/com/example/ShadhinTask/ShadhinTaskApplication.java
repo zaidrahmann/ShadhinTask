b25c6558c47ea43db3ac645a7a2a74a0dd808179
Zaid Rahman <zaidrahmann@gmail.com>
2025-10-21 18:49:51 +0600

    Initial commit: setup Spring Boot project and dependencies

---
+++ src/main/java/com/example/ShadhinTask/ShadhinTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShadhinTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShadhinTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShadhinTaskApplication.class, args);
+	}
+
+}
