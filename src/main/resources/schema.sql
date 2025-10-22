1da25011f386b5864560cf0fa467309a2d0ed241
Zaid Rahman <zaidrahmann@gmail.com>
2025-10-22 08:36:35 +0600

    I removed credentials and switched to env variables for security

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
+    gtitle VARCHAR(255) NOT NULL,
     description TEXT,
     priority INT NOT NULL,
     completed BOOLEAN NOT NULL DEFAULT FALSE
